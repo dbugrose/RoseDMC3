2377d994bbab0badb4a554ed964845dfc5a25e79
dbugrose <darcy.ophelia.rose@gmail.com>
2026-01-23 10:12:22 -0800

    url update

--- API/obj/Release/net9.0/API.AssemblyInfo.cs
+++ API/obj/Release/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6b3541eb4d01aee5edec884c758ada3614244ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56249b62cf523a6bf675e119d43d2a1852170fc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
